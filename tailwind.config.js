e3ff0dbda5270a71512a7431da63b523341f4697
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-11-24 08:42:09 +0000

    initial project set up done

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: '#002B5B',
+          secondary: '#2B4865',
+          accent: "#16213E",
+          "base-100": "#FFFFFF",
+        }
+      }
+    ]
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
