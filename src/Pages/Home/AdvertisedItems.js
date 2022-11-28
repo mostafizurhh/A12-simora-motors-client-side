dd8318a544c68c9c18f5a8242f33dd94acc81ab4
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-11-28 09:11:08 +0000

    hide advertised items from  home page

--- src/Pages/Home/AdvertisedItems.js
+++ src/Pages/Home/AdvertisedItems.js
@@ -1,10 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import AdvertisedItem from './AdvertisedItem';
 import Spinner from '../Shared/Spinner/Spinner';
-import { useContext, useState, useEffect } from 'react';
+import { useState } from 'react';
 import ItemBookModal from './ItemBookModal';
 import { Link } from 'react-router-dom';
-import { AuthContext } from '../../Contexts/AuthContext/AuthProvider';
 
 
 const AdvertisedItems = () => {
@@ -25,7 +24,7 @@ const AdvertisedItems = () => {
         return <Spinner></Spinner>
     }
 
-    if (advertisedItems.length > 0) {
+    if (advertisedItems.saleStatus === 'Available') {
         return (
             <div className='text-center'>
                 <h2 className='text-2xl font-bold mt-16 text-primary text-center'>Advertised items</h2>
